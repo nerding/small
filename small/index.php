88d3685044952e886defba699c3d2d0348269634
Don Kuntz <don@dkuntz2.com>
2013-05-05 20:03:37 -0500

    index change?

--- small/index.php
+++ small/index.php
@@ -1,11 +1,12 @@
 <?php
 	
-	require_once( dirname(__FILE__) . '/small.php' );
+	require_once( __dir__ . '/small.php' );
 
 	if (!User::isLoggedIn()) {
 		header("Location: login.php");
 	}
 
+  include(__dir__ . '/../theme/head.php');
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +15,20 @@
 	<title>Small Administration</title>
 </head>
 <body>
+  <section class="admin">
+    <header>
+      <h1>Small Administration</h1>
+      <a id="logout" href="#">Logout</a>
+    </header>
 
+
+  </section>
+  
+  <script>
+    $("#logout").click(function(event) {
+      event.preventDefault();
+        $.get("../ajax.php?action=logout", function(data) { location.reload(); });
+    });
+  </script>
 </body>
 </html>

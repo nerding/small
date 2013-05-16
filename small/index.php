0d8d18f259dde4703440ec417abe6036c93783c6
Don Kuntz <don@dkuntz2.com>
2013-05-16 16:14:30 -0500

    Played with Admin area again
    The admin area is starting to be nice. By starting I mean the cool developer-y
    things are looking nice, the actual interface is just a bar with changing
    content depending on which pseudo-tab you click.
    
    And on the pseudo-tab front, they work, again. And, thanks to history.pushState
    and window.onpopstate the forward and backward buttons in your browser (and on
    your five+ button mouse) also work inside of the admin interface. Which is nice,
    because I don't want to maintain more than one page at this point.
    
    They probably won't be used for the entire site, because actual pages make sense
    for public facing content, but for the administrative section, one make sense to
    me.
    
    On the login section, while I really wanted to modularize as many components as
    possible, that doesn't seem like it's going to happen, because of JavaScripts
    gorram differed. I want blocking, I'm not going to get blocking. That's all...

--- small/index.php
+++ small/index.php
@@ -6,29 +6,77 @@
 		header("Location: login.php");
 	}
 
+  $title = "Small Administration";
   include(__dir__ . '/../theme/head.php');
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Small Administration</title>
-</head>
-<body>
-  <section class="admin">
-    <header>
-      <h1>Small Administration</h1>
-      <a id="logout" href="#">Logout</a>
-    </header>
+<body class="admin">
+  <div class="header-bar">
+    <ul id="tabs-links">
+      <li><a href="#home">Small</a></li>
+      <li><a href="#write">Write</a></li>
+      <li><a href="#revise">Revise</a></li>
+      <li><a href="#manage">Manage</a></li>
+      <li><span id="logout">Logout</span></li>
+    </ul>
+  </div>
 
+  <div class="wrap">
+    <div id="tabs">
+      <section id="home">
+        Home
+      </section>
 
-  </section>
+      <section id="write">
+        Write
+        <form id="writePost">
+          <input type="text" placeholder="Title" id="postTitle" name="postTitle" />
+          
+          <textarea id="postContents" name="postContents" placeholder="Just Start Writing"></textarea>
+        </form>
+      </section>
+
+      <section id="revise">
+        Revise
+      </section>
+
+      <section id="manage">
+        Manage
+      </section>
+    </div>
+  </div>
   
   <script>
-    $("#logout").click(function(event) {
-      event.preventDefault();
+    function showTab(section) {
+      $("#tabs").find('section').hide();
+      $(section).show();
+    }
+
+    window.onpopstate = function() {
+      if (window.location.hash)
+        showTab(window.location.hash);
+      else
+        showTab('#home');
+    }
+    
+    $(document).ready(function() {
+      $("#logout").click(function(event) {
+        event.preventDefault();
         $.get("../ajax.php?action=logout", function(data) { location.reload(); });
-    });
+      });
+
+      // tabs
+      $('#tabs-links').find('a').click(function(event) {
+        event.preventDefault();
+        showTab($(this).attr('href'));
+        history.pushState(null, $(this).attr('href'), $(this).attr('href'));
+      });
+
+      showTab('#home');
+      if (window.location.hash)
+        showTab(window.location.hash);
+    })
+ 
   </script>
 </body>
 </html>
